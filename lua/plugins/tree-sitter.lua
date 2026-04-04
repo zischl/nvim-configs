d8f35f4b75cd3785101679a4dcc2f57e17c47e2d
zischl <supermechs456@gmail.com>
2026-04-04 11:53:16 +0530

    Adding go to default support

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,44 +1,45 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "javascript",
-        "typescript",
-        "tsx",
-        "json",
-        "html",
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true,
+			},
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"javascript",
+				"typescript",
+				"tsx",
+				"json",
+				"html",
+				"go",
+			},
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
 
-      -- if require('nvim-treesitter.indent').get_indent then
-      --   vim.filetype.add({
-      --     extension = {
-      --       jsx = {
-      --         indent_func = require('nvim-treesitter.indent').get_indent,
-      --       },
-      --       tsx = {
-      --         indent_func = require('nvim-treesitter.indent').get_indent,
-      --       },
-      --     },
-      --   })
-      -- end
-    end,
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
+			-- if require('nvim-treesitter.indent').get_indent then
+			--   vim.filetype.add({
+			--     extension = {
+			--       jsx = {
+			--         indent_func = require('nvim-treesitter.indent').get_indent,
+			--       },
+			--       tsx = {
+			--         indent_func = require('nvim-treesitter.indent').get_indent,
+			--       },
+			--     },
+			--   })
+			-- end
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 }
